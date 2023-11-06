3ee24c5264903b01bccff72683aafb47300b3a41
AsianManiac <asianmaniac2002@gmail.com>
2023-11-06 14:49:25 +0100

    added data and modified gitignore

---
+++ data/slider.ts
@@ -0,0 +1,69 @@
+export interface HeroSliderProps {
+    slider: any;
+    title: String;
+    url: String;
+
+}
+
+export const heroSlider: HeroSliderProps[] = [
+    {
+        slider: "/hero-slider/6Our_Walk_Home.png",
+        title: "Our Walk Home",
+        url: "https://www.webtoons.com/en/romance/our-walk-home/list?title_no=3899",
+    },
+    {
+        slider: "/hero-slider/4_GIF_SchoolBusGraveYard_091123_PC.gif",
+        title: "School Bus GraveYard",
+        url: "https://www.webtoons.com/en/thriller/school-bus-graveyard/list?title_no=2705",
+    },
+    {
+        slider: "/hero-slider/6Retired-Demon-King_Banner_Launch_A_Desktop.png",
+        title: "Retired Demon King",
+        url: "https://www.webtoons.com/en/romance/retired-demon-king/list?title_no=5111",
+    },
+    {
+        slider: "/hero-slider/0HelloBaby_desktopbanner_launch-nobadge_C.png",
+        title: "Hello Baby",
+        url: "https://www.webtoons.com/en/romance/hello-baby/list?title_no=5166",
+    },
+    {
+        slider: "/hero-slider/4PC_Warhaven_vendgeful_from_war_to_haven.png",
+        title: "Warhaven vengefull",
+        url: "https://www.webtoons.com/en/action/warhaven-vengeful/list?title_no=5724",
+    },
+    {
+        slider: "/hero-slider/3GAR_go_away_romeo.png",
+        title: "Go Away Romeo",
+        url: "https://www.webtoons.com/en/romance/go-away-romeo/list?title_no=5066",
+    },
+    {
+        slider: "/hero-slider/9DuncanandEddie_desktopbanner_220922.png",
+        title: "Duncan And Eddie",
+        url: "https://www.webtoons.com/en/challenge/duncan-and-eddie/list?title_no=759764",
+    },
+    {
+        slider: "/hero-slider/I-want-to-be-a-cute-anime-girl_desktop_banner_1_06232020.png",
+        title: "I Want To Be a Cute Anime Girl",
+        url: "https://www.webtoons.com/en/challenge/i-want-to-be-a-cute-anime-girl/list?title_no=349416",
+    },
+    {
+        slider: "/hero-slider/2Omniscient+Reader_Refresh_desktopbanner_Ver1.png",
+        title: "Omniscient Reader",
+        url: "https://www.webtoons.com/en/action/omniscient-reader/list?title_no=2154",
+    },
+    {
+        slider: "/hero-slider/8OfSwamp_Sea_desktopbanner_purpose_A.png",
+        title: "Of Swamp And Sea",
+        url: "https://www.webtoons.com/en/supernatural/of-swamp-and-sea/list?title_no=2736",
+    },
+    {
+        slider: "/hero-slider/8OnlyHope_desktopbanner_launch-nobadge_B.png",
+        title: "Only Hope",
+        url: "https://www.webtoons.com/en/romance/only-hope/list?title_no=5036",
+    },
+    {
+        slider: "/hero-slider/5RAaUH_desktopbanner_tango_110921.png",
+        title: "Reincarnated As An Unruly Heir",
+        url: "https://www.webtoons.com/en/action/reincarnated-as-an-unruly-heir/list?title_no=3597",
+    },
+]
